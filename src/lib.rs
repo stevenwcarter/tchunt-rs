89863ebb306a3cca1a7f3b294846d4d97649c1be
Steven Carter <steve@javapl.us>
2024-04-04 17:11:19 -0400

    feat: only show types without an identified mime type, others are on TRACE level

--- src/lib.rs
+++ src/lib.rs
@@ -1,78 +1,49 @@
 use anyhow::{Context, Result};
-use async_walkdir::{Filtering, WalkDir};
-use futures_lite::stream::StreamExt;
+use async_walkdir::WalkDir;
+use futures::stream::StreamExt;
+use infer::Type;
 use log::*;
 use std::sync::Arc;
-use std::thread;
 use tokio::fs::File;
 use tokio::io::{AsyncReadExt, AsyncSeekExt, BufReader};
-use tokio::sync::mpsc;
 use tokio::sync::Mutex;
 
 pub async fn search_dir(directory: &'static str) -> Result<()> {
     let finished = Arc::new(Mutex::new(false));
     let thread_finished = finished.clone();
 
-    let handle = Arc::new(tokio::runtime::Handle::current());
-    let thread_handle = handle.clone();
-
-    let (tx, mut rx) = mpsc::channel(100);
-    let search_thread = thread::spawn(move || {
-        thread_handle.block_on(async move {
-            let mut counter = 0;
-            let start = std::time::Instant::now();
-            let mut entries = WalkDir::new(directory).filter(|entry| async move {
-                if let Ok(metadata) = tokio::fs::metadata(entry.path().display().to_string()).await
-                {
-                    if metadata.is_file() {
-                        let length = metadata.len();
-                        if length % 512 == 0 && length > 1024 * 1024 * 30 {
-                            return Filtering::Continue;
-                        } else {
-                            return Filtering::Ignore;
-                        }
-                    }
-                }
-                Filtering::Continue
-            });
-
-            loop {
-                match entries.next().await {
-                    Some(Ok(entry)) => {
-                        counter += 1;
-                        let tx = tx.clone();
-                        let entry_path = entry.path().display().to_string();
-                        tx.send(entry_path).await.expect("Failed to send path");
-                    }
-                    Some(Err(_e)) => {
-                        // eprintln!("error: {}", e);
-                    }
-                    None => break,
-                }
-            }
-
-            let duration = start.elapsed();
-            info!("Time taken to find entries: {:?}", duration);
-            info!("Checked {} files", counter);
-            *thread_finished.lock().await = true;
-        })
-    });
-
-    let executor = tokio::task::spawn(async move {
+    let search_thread = tokio::spawn(async move {
+        let mut counter = 0;
         let start = std::time::Instant::now();
-        while !*finished.lock().await {
-            while let Some(path) = rx.recv().await {
-                let _ = check_file(&path).await;
+        let mut entries = WalkDir::new(directory); //.filter(screen_file);
+
+        loop {
+            match entries.next().await {
+                Some(Ok(entry)) => {
+                    counter += 1;
+                    if counter % 1000 == 0 {
+                        let duration = start.elapsed();
+                        trace!("Scanned {counter} files in {:?}", duration);
+                    }
+                    // if screen_file(&entry).await {
+                    let entry_path = entry.path().display().to_string();
+                    let _ = check_file(&entry_path).await;
+                    // }
+                }
+                Some(Err(_e)) => {
+                    // eprintln!("error: {}", e);
+                }
+                None => break,
             }
         }
+
         let duration = start.elapsed();
-        info!("Time taken to scan entries: {:?}", duration);
+        info!("Time taken to find entries: {:?}", duration);
+        info!("Checked {} files", counter);
+        *thread_finished.lock().await = true;
     });
 
-    executor.await.expect("Executor thread panicked");
-    info!("Started processing thread");
-    search_thread.join().expect("Search thread panicked");
-    info!("Started search thread");
+    let _ = search_thread.await;
 
     Ok(())
 }
@@ -82,7 +53,7 @@ pub async fn check_file(filename: &str) -> Result<()> {
         .await
         .context("Could not open file")?;
     let length = metadata.len();
-    if length % 512 != 0 || length < 512 {
+    if metadata.is_dir() || length % 512 != 0 || length < 1024 * 1024 * 10 {
         return Ok(());
     }
     let mut file = File::open(filename)
@@ -95,10 +66,25 @@ pub async fn check_file(filename: &str) -> Result<()> {
     if shannon < 7.9 {
         return Ok(());
     }
+    let file_type = get_file_type(filename).await;
 
-    println!("{} {}", shannon, filename);
+    match file_type {
+        Ok(Some(found_type)) => {
+            trace!("(possible) {} {} {:?}", shannon, filename, found_type);
+        }
+        Ok(None) => {
+            println!("{} {}", shannon, filename);
+        }
+        Err(e) => {
+            error!("Error checking file type for {filename} {:?}", e);
+        }
+    }
 
     Ok(())
+}
+
+pub async fn get_file_type(filename: &str) -> Result<Option<Type>> {
+    infer::get_from_path(filename).context("could not read file for type inference")
 }
 
 pub async fn shannon_entropy(file: &mut tokio::fs::File) -> Result<f32> {
