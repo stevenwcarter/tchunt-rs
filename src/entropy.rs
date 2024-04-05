93476751eca5c3b41da8a5f1992ee3a678a52eb4
Steven Carter <steve@javapl.us>
2024-04-05 18:49:48 -0400

    feat: creating entropy struct

--- src/entropy.rs
+++ src/entropy.rs
@@ -1,45 +1,109 @@
 use anyhow::Result;
+use std::io::Cursor;
 use tokio::io::{AsyncReadExt, AsyncSeekExt, BufReader};
 
-pub async fn shannon(file: &mut tokio::fs::File) -> Result<f32> {
-    let mut entropy = 0.0;
-    let mut counts = [0usize; 256];
+pub struct Entropy<'a, R> {
+    source: &'a mut R,
+    reader: BufReader<R>,
+    length: usize,
+}
 
-    let mut bytes_to_read: usize = 171072;
-    let file_len: usize = file.metadata().await?.len() as usize;
-    if file_len < bytes_to_read {
-        bytes_to_read = file_len;
+impl<'a> Entropy<'a, tokio::fs::File> {
+    pub async fn new_from_file(file: &'a mut tokio::fs::File) -> Result<Self> {
+        let length: usize = file.metadata().await?.len() as usize;
+        let reader = BufReader::new(file.try_clone().await?);
+
+        Ok(Self {
+            source: file,
+            reader,
+            length,
+        })
     }
-    let mut bytes_read = bytes_to_read;
+}
+impl<'a> Entropy<'a, Cursor<Vec<u8>>> {
+    pub async fn new_from_vec(cursor: &'a mut Cursor<Vec<u8>>, length: usize) -> Result<Self> {
+        let reader = BufReader::new(cursor.clone());
 
-    let mut buffer = vec![0; bytes_to_read];
-    let mut reader = BufReader::new(file.try_clone().await?);
-    reader.read_exact(&mut buffer).await?;
-
-    for byte in buffer {
-        counts[byte as usize] += 1;
+        Ok(Self {
+            source: cursor,
+            reader,
+            length,
+        })
     }
-    if file_len > 2 * bytes_to_read {
+}
+
+impl<'a, R> Entropy<'a, R> {
+    pub async fn shannon(&mut self) -> Result<f32>
+    where
+        R: AsyncReadExt + AsyncSeekExt + Unpin,
+    {
+        let mut entropy = 0.0;
+        let mut counts = [0usize; 256];
+
+        let mut bytes_to_read: usize = 171072;
+        let file_len: usize = self.length;
+        if file_len < bytes_to_read {
+            bytes_to_read = file_len;
+        }
+        let mut bytes_read = bytes_to_read;
+
         let mut buffer = vec![0; bytes_to_read];
-        file.seek(tokio::io::SeekFrom::Start(
-            (file_len - bytes_to_read) as u64,
-        ))
-        .await?;
-        reader.read_exact(&mut buffer).await?;
+        self.reader.read_exact(&mut buffer).await?;
+
         for byte in buffer {
             counts[byte as usize] += 1;
         }
-        bytes_read += bytes_to_read;
-    }
-
-    for &count in &counts {
-        if count == 0 {
-            continue;
+        if file_len > 2 * bytes_to_read {
+            let mut buffer = vec![0; bytes_to_read];
+            self.source
+                .seek(tokio::io::SeekFrom::Start(
+                    (file_len - bytes_to_read) as u64,
+                ))
+                .await?;
+            self.reader.read_exact(&mut buffer).await?;
+            for byte in buffer {
+                counts[byte as usize] += 1;
+            }
+            bytes_read += bytes_to_read;
         }
 
-        let p: f32 = (count as f32) / (bytes_read as f32);
-        entropy -= p * p.log(2.0);
-    }
+        for &count in &counts {
+            if count == 0 {
+                continue;
+            }
 
-    Ok(entropy)
+            let p: f32 = (count as f32) / (bytes_read as f32);
+            entropy -= p * p.log(2.0);
+        }
+
+        Ok(entropy)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn it_calculates_entropy_correctly() {
+        let vec = vec![0; 200];
+        let len = vec.len();
+        let mut cursor = Cursor::new(vec);
+
+        let mut entropy = Entropy::new_from_vec(&mut cursor, len).await.unwrap();
+
+        assert_eq!(entropy.shannon().await.unwrap(), 0.0);
+    }
+    #[tokio::test]
+    async fn it_calculates_entropy_for_high_entropy_sources() {
+        use rand::Rng;
+        let mut rng = rand::thread_rng();
+        let vec: Vec<u8> = (0..2000000).map(|_| rng.gen_range(0..255)).collect();
+        let len = vec.len();
+        let mut cursor = Cursor::new(vec);
+
+        let mut entropy = Entropy::new_from_vec(&mut cursor, len).await.unwrap();
+
+        assert!(entropy.shannon().await.unwrap() > 7.8);
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,40 @@
 use anyhow::{Context, Result};
 use async_walkdir::WalkDir;
-use futures::stream::StreamExt;
-use infer::Type;
+use futures_lite::stream::StreamExt;
 use log::*;
-use std::sync::Arc;
 use tokio::fs::File;
-use tokio::sync::Mutex;
 
 pub mod entropy;
 
-pub async fn search_dir(directory: &'static str) -> Result<()> {
-    let finished = Arc::new(Mutex::new(false));
-    let thread_finished = finished.clone();
+pub async fn search_dir(directory: &'static str) {
+    let mut counter = 0;
+    let start = std::time::Instant::now();
+    let mut entries = WalkDir::new(directory); //.filter(screen_file);
 
-    let search_thread = tokio::spawn(async move {
-        let mut counter = 0;
-        let start = std::time::Instant::now();
-        let mut entries = WalkDir::new(directory); //.filter(screen_file);
-
-        loop {
-            match entries.next().await {
-                Some(Ok(entry)) => {
-                    counter += 1;
-                    if counter % 1000 == 0 {
-                        let duration = start.elapsed();
-                        trace!("Scanned {counter} files in {:?}", duration);
-                    }
-                    // if screen_file(&entry).await {
-                    let entry_path = entry.path().display().to_string();
-                    let _ = check_file(&entry_path).await;
-                    // }
+    loop {
+        match entries.next().await {
+            Some(Ok(entry)) => {
+                counter += 1;
+                if counter % 10000 == 0 {
+                    let duration = start.elapsed();
+                    trace!("Scanned {counter} files in {:?}", duration);
                 }
-                Some(Err(_e)) => {
-                    // eprintln!("error: {}", e);
-                }
-                None => break,
+                let entry_path = entry.path().display().to_string();
+                let _ = check_file(&entry_path).await;
             }
+            Some(Err(_e)) => {
+                // ignore files we cannot open
+            }
+            None => break,
         }
+    }
 
-        let duration = start.elapsed();
-        info!("Time taken to find entries: {:?}", duration);
-        info!("Checked {} files", counter);
-        *thread_finished.lock().await = true;
-    });
-
-    let _ = search_thread.await;
-
-    Ok(())
+    let duration = start.elapsed();
+    info!("Time taken to find entries: {:?}", duration);
+    info!("Checked {} files", counter);
 }
 
-pub async fn check_file(filename: &str) -> Result<()> {
+async fn check_file(filename: &str) -> Result<()> {
     let metadata = tokio::fs::metadata(filename)
         .await
         .context("Could not open file")?;
@@ -61,13 +46,16 @@ pub async fn check_file(filename: &str) -> Result<()> {
         .await
         .context("Could not open file for tokio")?;
 
-    let shannon = entropy::shannon(&mut file)
+    let mut entropy = entropy::Entropy::new_from_file(&mut file).await?;
+    let shannon = entropy
+        .shannon()
         .await
         .context("could not get shannon entropy")?;
-    if shannon < 7.9 {
+    if shannon < 7.95 {
         return Ok(());
     }
-    let file_type = get_file_type(filename).await;
+
+    let file_type = infer::get_from_path(filename);
 
     match file_type {
         Ok(Some(found_type)) => {
@@ -82,8 +70,4 @@ pub async fn check_file(filename: &str) -> Result<()> {
     }
 
     Ok(())
-}
-
-pub async fn get_file_type(filename: &str) -> Result<Option<Type>> {
-    infer::get_from_path(filename).context("could not read file for type inference")
 }
